cab47183ab418888ed89eb5255d286a4a123b239
FaceNach <ignacioijg@gmail.com>
2024-03-22 19:23:29 -0300

    First steps of Api

---
+++ API DotNet8/Controllers/SuperHeroController.cs
@@ -0,0 +1,29 @@
+﻿using API_DotNet8.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_DotNet8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id =1,
+                    Name = "Spiderman",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                }
+            };
+
+            return Ok(heroes);
+        }
+    }
+}
---
+++ API DotNet8/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using API_DotNet8.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_DotNet8.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
